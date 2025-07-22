c2c7bec396006c5b8f9f06243f7ebee1af03cfb3
insidetail <insidetail@monoshare.jp>
2025-07-22 11:56:38 +0900

    main.py modified

--- main.py
+++ main.py
@@ -99,5 +99,5 @@ async def create_checkout_session(request: Request, user=Depends(get_current_use
 if __name__ == '__main__':
     import os
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
