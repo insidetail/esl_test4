e28fe2c03902ac83743d293b69e80650a254d426
insidetail <insidetail@monoshare.jp>
2025-07-22 12:09:35 +0900

    main.py Debug log setting

--- main.py
+++ main.py
@@ -100,4 +100,10 @@ if __name__ == '__main__':
     import os
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        log_level="debug",    # ← DEBUG レベルで詳細ログ取得
+    )
+
